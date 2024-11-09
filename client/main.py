e410730257bf11e7e49014f2535c5b8ddc0248c8
haleemairfan <haleemairfan04@gmail.com>
2024-11-09 17:01:54 +0800

    View reminders

--- client/main.py
+++ client/main.py
@@ -35,8 +35,31 @@ async def helpCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Type your tasks into this bot')
 
 
-async def customCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('This is a custom command')
+async def viewCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    current_date = datetime.now().date()
+    days_until_end_of_week = 6 - current_date.weekday()
+
+
+    buttons = []
+
+    for i in range(days_until_end_of_week + 1):
+        day = current_date + timedelta(days=i)
+        day_str = day.strftime('%A, %d %B')
+        buttons.append([InlineKeyboardButton(day_str, callback_data=f'view_reminder_{day}')])
+    
+    response = InlineKeyboardMarkup(buttons)
+    await update.message.reply_text(
+        "Let’s view your reminders", reply_markup=response
+    )
+
+async def setCommand(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("Create my reminders!", callback_data='create_reminders')]
+    ]
+    response = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(
+        "Let's set some reminders!", reply_markup=response
+    )
 
 
 # Callback query handler for button clicks
@@ -54,18 +77,30 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
     elif query.data  == 'store_reminder':
         context.user_data['awaiting_reminder'] = True
         await query.message.reply_text('Please enter your reminder')
+    elif query.data == 'exit':
+        await exit(update, context)
+    elif query.data.startswith('view_reminder_'):
+        selected_date_str = query.data.replace('view_reminder_', '')
+        selected_date = datetime.strptime(selected_date_str, "%Y-%m-%d").date()
+        await view_reminder(update, context, selected_date)
+            
 
 async def set_reminder(update: Update, context: ContextTypes.DEFAULT_TYPE, selected_date: datetime.date):
     formatted_date = selected_date.strftime("%A, %d %B %Y")
     keyboard = [
         [InlineKeyboardButton("Enter your reminder!", callback_data='store_reminder')],
-        [InlineKeyboardButton("I have no more reminders!", callback_data='select_day')]
+        [InlineKeyboardButton("I have no more reminders!", callback_data='create_reminders')]
     ]
 
     response = InlineKeyboardMarkup(keyboard)
-    await update.callback_query.message.reply_text(
-        f'Please enter your reminders for {formatted_date}:', reply_markup=response
-    )
+    if update.callback_query:
+        await update.callback_query.message.reply_text(
+            f'Please enter your reminders for {formatted_date}:', reply_markup=response
+        )
+    else:
+        await update.message.reply_text(
+            f'Please enter your reminders for {formatted_date}:', reply_markup=response
+        )
 
 async def store_reminder(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.user_data.get('awaiting_reminder'):
@@ -80,7 +115,7 @@ async def store_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if context.user_data.get('awaiting_time'):
             text = update.message.text
             selected_time = datetime.strptime(text, "%H:%M").time()
-            context.user_data['awaiting time'] = False
+            context.user_data['awaiting_time'] = False
             selected_date = context.user_data.get('selected_date')
 
             reminder_data = {
@@ -88,20 +123,49 @@ async def store_time(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 "date": selected_date.strftime("%Y-%m-%d"),
                 "time": selected_time.strftime("%H:%M")
             }
-
-            print(f"Reminder stored: {reminder_data}")
-            await update.message.reply_text(f"Reminder saved: {reminder_data['content']} on {reminder_data['date']} at {reminder_data['time']}")
+            response = requests.post("https://reminder-bot-api.onrender.com/storeReminders",
+                          json=reminder_data)
+            if response.status_code == 201:
+                await update.message.reply_text(f"Reminder saved: {reminder_data['content']} on {reminder_data['date']} at {reminder_data['time']}")
+            else:
+                await update.message.reply_text(f"Failed to save reminder: {response.status_code}")
 
             # Clear the state
-            context.user_data.clear()
+            context.user_data.pop('content', None)
+            context.user_data.pop('awaiting_time', None)
+            context.user_data.pop('awaiting_reminder', None)
 
-
-
+            await set_reminder(update, context, selected_date)
     except ValueError:
         await update.message.reply_text("Invalid time format. Please enter the time in HH:MM format.")
 
-
+async def view_reminder(update: Update, context: ContextTypes.DEFAULT_TYPE, selected_date: datetime):
+    try:
+        response = requests.get(f"https://reminder-bot-api.onrender.com/viewReminders?date={selected_date.strftime('%Y-%m-%d')}")
         
+        if response.status_code == 200:
+            # Parse the response data
+            data = response.json()
+            reminders = data.get("data", [])
+            
+            if reminders:
+                reminder_string = [f"- {reminder['time']}: {reminder['content']}" for reminder in reminders]
+                message = f"Reminders for {selected_date.strftime('%A, %d %B %Y')}:\n" + "\n".join(reminder_string)
+            else:
+                message = f"No reminders found for {selected_date.strftime('%A, %d %B %Y')}."
+            
+            await update.callback_query.message.reply_text(message)
+        else:
+            await update.callback_query.message.reply_text(
+                f"Failed to retrieve reminders. Status code: {response.status_code}. Response: {response.text}"
+            )
+    except Exception as e:
+        await update.callback_query.message.reply_text(f"An error occurred: {str(e)}")
+
+
+async def exit(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.message.reply_text("Okay! See ya next time!")
+    
 
 # Responses
 async def handleResponse(text: str) -> str:
@@ -156,6 +220,8 @@ async def handleReminderCreation(update: Update, context: ContextTypes.DEFAULT_T
         day = current_date + timedelta(days=i)
         day_str = day.strftime('%A, %d %B')
         buttons.append([InlineKeyboardButton(day_str, callback_data=f'set_reminder_{day}')])
+
+    buttons.append([InlineKeyboardButton("Exit", callback_data='exit')])
     
     response = InlineKeyboardMarkup(buttons)
     await update.callback_query.message.reply_text(
@@ -176,8 +242,9 @@ if __name__ == '__main__':
 
     # Commands
     app.add_handler(CommandHandler('start', startCommand))
+    app.add_handler(CommandHandler('set', setCommand))
+    app.add_handler(CommandHandler('view', viewCommand))
     app.add_handler(CommandHandler('help', helpCommand))
-    app.add_handler(CommandHandler('custom', customCommand))
 
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handleMessage))
