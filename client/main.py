7f31245c0533fd2ce41b7ae33951ae9b78769cbd
haleemairfan <haleemairfan04@gmail.com>
2024-11-17 03:48:25 +0800

    Add handling of HEAD requests

--- client/main.py
+++ client/main.py
@@ -233,6 +233,10 @@ class simpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Bot is running.")
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 
 def run_dummy_server():
     port = int(os.environ.get("PORT", 5001))
